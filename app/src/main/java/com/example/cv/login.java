80bb9c00d58042b8844e14c4ac18008d39a6a02a
Login <mstrhans.zamora@gmail.com>
2024-10-25 03:44:27 +0800

    updated login

--- app/src/main/java/com/example/cv/login.java
+++ app/src/main/java/com/example/cv/login.java
@@ -1,5 +1,6 @@
 package com.example.cv;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,12 +11,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.annotations.Nullable;
+
+import java.util.HashMap;
 
 public class login extends AppCompatActivity {
 
@@ -25,6 +42,19 @@ public class login extends AppCompatActivity {
     private ProgressBar progressBar;
     private FirebaseAuth mAuth;
     private TextView textViewLoginNow;
+
+   private Button google_sign_in_btn;
+
+    FirebaseAuth firebaseAuth;
+
+    FirebaseDatabase database;
+
+    GoogleSignInOptions gso;
+
+    GoogleSignInClient gsc;
+    int RC_SIGN_IN = 20;
+
+
 
     @Override
     public void onStart() {
@@ -42,6 +72,7 @@ public class login extends AppCompatActivity {
         }
     }
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,6 +93,23 @@ public class login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 signUpUser();
+            }
+        });
+
+        google_sign_in_btn = findViewById(R.id.google_sign_in_btn);
+        firebaseAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+
+        gsc = GoogleSignIn.getClient(login.this,gso);
+
+        google_sign_in_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
             }
         });
 
@@ -87,6 +135,52 @@ public class login extends AppCompatActivity {
             mp.setLooping(true);  // Set video looping
             videoView.start();    // Start the video
         });
+    }
+    private void signIn() {
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent,RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuth(account.getIdToken());
+            }catch (Exception e){
+                Toast.makeText(this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    private void firebaseAuth(String idToken) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            HashMap<String, Object> map = new HashMap<>();
+                            map.put("id",user.getUid());
+                            map.put("name",user.getDisplayName());
+                            map.put("profile",user.getPhotoUrl().toString());
+                            database.getReference().child("userss").child(user.getUid()).setValue(map);
+
+                            Intent intent = new Intent(login.this, HomePage.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(login.this, "Something Went Wrong!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private void signUpUser() {
